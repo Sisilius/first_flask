87e34a321e4e3c554ddbbd4621db91b335ac803c
Sisilius <42961373+Sisilius@users.noreply.github.com>
2020-09-06 22:48:08 +0500

    create DB

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask (__name__)
+# Вся байда с БД
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+# Поехали таблицы
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(50), nullable = False)
+    intro = db.Column(db.String(200), nullable = False)
+    text = db.Column(db.Text, nullable = False)
+    date = db.Column(db.DateTime, default = datetime.utcnow)
+# типа возвращает объект и его id
+    def __repr__(self):
+        return '<Article %r>' %self.id
+
 
 @app.route('/')
 @app.route('/home')
@@ -10,6 +27,25 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/create_article', methods = ['POST', 'GET'])
+def create_article():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Ошибка при добавлении"
+    else:
+        return render_template('create_article.html')
+
 
 @app.route('/user/<string:name>/<int:id>') #Передаём кастоымные значения через адрессную строку
 def user(name, id):
