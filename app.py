ade6388bc95e9841a8f5147b37fe193d75244438
Sisilius <42961373+Sisilius@users.noreply.github.com>
2020-09-06 12:32:24 +0500

    add html

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask (__name__)
 
 @app.route('/')
+@app.route('/home')
 def index():
-    return 'Привет, мир!'
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-    return 'Страница "О нас"'
+    return render_template('about.html')
+
+@app.route('/user/<string:name>/<int:id>') #Передаём кастоымные значения через адрессную строку
+def user(name, id):
+    return f'Страница пользователя {name} с id {id}'
+
 
 if __name__ == '__main__':
     app.run(debug = True)
