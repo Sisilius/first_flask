87ea474e2663909d8483f2d0ea3b2444c83b25ae
Sisilius <42961373+Sisilius@users.noreply.github.com>
2020-09-07 22:12:46 +0500

    add edit and remove posts

--- app.py
+++ app.py
@@ -24,21 +24,53 @@ class Article(db.Model):
 def index():
     return render_template('index.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 @app.route('/posts')
 def posts():
     articles = Article.query.order_by(Article.date.desc()).all()
     return render_template('posts.html', articles=articles)
 
+
 @app.route('/posts/<int:id>')
 def post_detail(id):
     article = Article.query.get(id)
     return render_template('post_detail.html', article=article)
 
-    
+
+@app.route('/posts/<int:id>/delete')
+def post_delete(id):
+    article = Article.query.get_or_404(id)
+
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        return "Ошибка при удалении"
+
+
+@app.route('/posts/<int:id>/update', methods = ['POST', 'GET'])
+def post_update(id):
+    article = Article.query.get(id)
+    if request.method == 'POST':
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
+
+        try:
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return "Ошибка при обновлении"
+    else:
+        return render_template('post_update.html', article=article)
+
+
 @app.route('/create_article', methods = ['POST', 'GET'])
 def create_article():
     if request.method == 'POST':
